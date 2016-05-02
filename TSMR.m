0bd2a4d917df942f4740c170f327633f3218b1c1
BH1TAN <yxtccty@163.com>
2016-05-02 23:44:59 +0800

    优化反馈

--- TSMR.m
+++ TSMR.m
@@ -1,11 +1,15 @@
 function TSMR(n1,n2,mb,deadt,times,mintime,maxtime,step)
 %进行最长时间为maxtime秒，步进为step的测量，并将结果显示
+global data
+
 i = mintime;
-j=1;
+j = 1;
 while(i<=maxtime)
     data(j) = TSM(n1,n2,mb,i,deadt,times);
-    i=i+step;
-    j=j+1;
+    i = i+step;
+    j = j+1;
+    fprintf('********\nPRESENT:%d\nMAX:%d\nSTEP:%d\n********\n\n',i,maxtime,step);
+    
 end
 plot(data);
 
---
+++ initmeasure.m
@@ -0,0 +1,13 @@
+function initmeasure(n1,n2,mb,deadt,times,mintime,maxtime,step)
+
+global n10 n20 mb0 deadt0 times0 mintime0 maxtime0 step0;
+n10=n1;
+n20=n2;
+mb0=mb;
+deadt0=deadt;
+times0=times;
+mintime0=mintime;
+maxtime0=maxtime;
+step0=step;
+
+end
